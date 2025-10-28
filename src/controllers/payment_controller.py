921f11ea01ab7bbb57b55e1cf627ed7dc3456057
Gabriel Look <gabriel.look.1@ens.etsmtl.ca>
2025-10-28 00:03:26 -0400

    Added order payment status update

--- src/controllers/payment_controller.py
+++ src/controllers/payment_controller.py
@@ -31,12 +31,32 @@ def process_payment(payment_id, credit_card_data):
     # Si le paiement est réussi, mettre à jour les statut de la commande
     # Ensuite, faire la mise à jour de la commande dans le Store Manager (en utilisant l'order_id)
     update_result = update_status_to_paid(payment_id)
-    print(f"Updated order {update_result['order_id']} to paid={update_result}")
+    result_order_id = update_result['order_id']
+    result_payment_id = update_result["payment_id"]
+    result_is_paid = update_result["is_paid"]
+    
     result = {
-        "order_id": update_result["order_id"],
-        "payment_id": update_result["payment_id"],
-        "is_paid": update_result["is_paid"]
+        "order_id": result_order_id,
+        "payment_id": result_payment_id,
+        "is_paid": result_is_paid
     }
+    
+    print(f"Updated order {result_order_id} to paid={update_result}")
+    
+    response = requests.put(
+        "http://api-gateway:8080/store-api/orders",
+        json={
+            "order_id": result_order_id,
+            "is_paid": True
+        },
+        headers={"Content-Type": "application/json"},
+        timeout=5
+    )
+    
+    if response.ok:
+        print(f"Commande {result_order_id} mise à jour avec succès (is_paid=True)")
+    else:
+        print(f"Erreur lors de la mise à jour de la commande {result_order_id}: {response.status_code} - {response.text}")
 
     return result
     
